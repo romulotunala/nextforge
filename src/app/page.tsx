0cf5d8ddebd7cd8cfdfaa3b8fc94cab45d194ae3
Romulo Tunala <romulotunala@gmail.com>
2026-07-09 17:58:21 -0300

    chore: initial bootstrap

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>nextforge</h1>
+      <p>Boilerplate para sites institucionais estáticos com Next.js.</p>
+    </main>
+  );
+}
---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'max-line-length': 100,
+    'string-quotes': 'single',
+  },
+};
